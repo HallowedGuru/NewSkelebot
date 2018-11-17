48a4ffab669ab69e391dc2df8ce67051dd7e5898
jfagan132 <jfagan@ggc.edu>
2018-11-17 18:48:12 -0500

    set game to help commands

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Commando.Client({
 	commandPrefix: '$'
 });
 
-const BOT_TOKEN = process.env.TOKEN;
+const BOT_TOKEN = "NDUzNzI4MDI3MTU1ODkwMTk2.Ds-3eA.8tOsQzbplbN3ZO3jWb-cV5vsYi4";
 
 bot.registry.registerGroup('simple', 'simple');
 bot.registry.registerGroup('conversion', 'conversion');
@@ -13,7 +13,7 @@ bot.registry.registerCommandsIn(__dirname + '/commands');
 bot.login(BOT_TOKEN);
 
 bot.on('ready', function(){
-	bot.user.setGame('$help');
+	bot.user.setGame('$help | @Skelebot help');
     console.log('Ready!');
 });
 
